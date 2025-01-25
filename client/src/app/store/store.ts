f1a636b628b59cef09bedec1098cd2ff2b755468
gc2321 <gchan.email@gmail.com>
2025-01-25 10:45:54 -0500

    end of section 5

---
+++ client/src/app/store/store.ts
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../../features/contact/counterReducer";
+import { useDispatch, useSelector } from "react-redux";
+
+import { uiSlice } from "../layout/uiSlice";
+import { catalogApi } from "../../features/catalog/catalogApi";
+
+export const store = configureStore({
+    reducer: {
+        [catalogApi.reducerPath]: catalogApi.reducer,
+        counter: counterSlice.reducer,
+        ui: uiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(catalogApi.middleware)
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
